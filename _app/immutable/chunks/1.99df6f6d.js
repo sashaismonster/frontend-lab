f95135b24732a241d70b6bbe1d27b0fb0be1e3d8
sashaismonster <a.golovin@innopolis.university>
2023-03-23 10:13:24 +0000

    Deploying to gh-pages from @ sashaismonster/frontend-lab@e0e79b3214d5e7f2405cb455dd264c5b35ed08f2 🚀

---
+++ _app/immutable/chunks/1.99df6f6d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.80d38d12.js";export{t as component};
---
+++ _app/immutable/chunks/3.74279bde.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page.1806d283.js";import{default as t}from"../entry/about-page.svelte.a26bf4d0.js";export{t as component,r as universal};
